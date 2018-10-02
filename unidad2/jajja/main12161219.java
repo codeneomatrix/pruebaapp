a879ef4e008b743bae6e77d3e6d8ee8fc6a506d9
codeneomatrix <josuecevedo@gmail.com>
2018-10-02 16:19:42 -0500

    tests

--- unidad2/jajja/main12161219.java
+++
@@ -1,5 +0,0 @@
-class main12161219 {
-    public static void main(String[] args) {
-        System.out.println("96");
-    }
-}
---
+++ unidad2/jajja/main12161220.java
@@ -0,0 +1,6 @@
+class main12161220 {
+    public static void main(String[] args) {
+    	int dato = Integer.parseInt(args[0]);
+        System.out.println(dato*dato);
+    }
+}
